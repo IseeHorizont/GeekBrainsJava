cd790990755cc830068f96d4933f93e4bb99c731
Evgeny <angst_eam@mail.ru>
2020-11-04 14:04:00 +1000

    myThirdHomeWorkOnJavaOne

--- src/JavaOneTaskOne.java
+++ src/JavaOneTaskOne.java
@@ -1,118 +1,77 @@
-import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class JavaOneTaskOne {
     public static void main(String[] args) {
+        guessRandomNumberSince1Until9();
 
-        // task1
-        int[] firstArr = {0, 1, 1, 1, 0, 0, 1, 1, 1, 0};
-        for(int i = 0; i < firstArr.length; i++){
-            if(firstArr[i] == 0){
-                firstArr[i] = 1;
+        guessRandomWords();
+    }
+
+    public static void guessRandomNumberSince1Until9(){
+        Scanner scan = new Scanner(System.in);
+        int randomNum = (int)(Math.random() * (9 - 1));
+
+        System.out.println("Угадайте число от 1 до 9. У Вас 3 попытки.");
+
+        while(true) {
+            for (int i = 1; i <= 3; i++) {
+                int inputNumb = scan.nextInt();
+                if (inputNumb == randomNum) {
+                    System.out.println("Победа! Вы угадали число!");
+                    return;
+                } else {
+                    System.out.println("Вы не угадали число.");
+                    if (inputNumb > randomNum) {
+                        System.out.println("Подсказка: \"Введённое число больше загаданного\"");
+                    } else {
+                        System.out.println("Подсказка: \"Введённое число меньше загаданного\"");
+                    }
+                    System.out.println("Осталось попыток: " + (3 - i));
+                }
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+            int inputAnswer = scan.nextInt();
+            if(inputAnswer == 0){
+                System.out.println("Good luck! See you next game...");
+                break;
             }else{
-                firstArr[i] = 0;
-            }
-            System.out.print(firstArr[i] + " ");
-        }
-
-        System.out.println('\n' + "- - - - - - - - - -");
-
-        // task2
-        int[] secondArr = new int[8];
-        for(int i = 0; i < secondArr.length; i++){
-            secondArr[i] = i*3;
-            System.out.print(secondArr[i] + " ");
-        }
-
-        System.out.println('\n' + "- - - - - - - - - -");
-
-        // task3
-        int[] thirdArr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for(int i = 0; i < thirdArr.length; i++){
-            if(thirdArr[i] < 6){
-                thirdArr[i] = thirdArr[i] * 2;
-            }
-            System.out.print(thirdArr[i] + " ");
-        }
-
-        System.out.println('\n' + "- - - - - - - - - -");
-
-        //task4
-        int[][] fourthArr = new int[4][4];
-        for(int i = 0; i < fourthArr.length; i++){
-            for(int j = 0; j < fourthArr.length; j++){
-                if(i == j || i+j == 3){
-                    fourthArr[i][j] = 1;
-                }
-                System.out.print(fourthArr[i][j]  + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("- - - - - - - - - -");
-
-        // task5
-        int[] arrForMinAndMax = thirdArr;
-        int max = arrForMinAndMax[0];
-        int min = 100;
-        for(int i = 0; i < arrForMinAndMax.length; i++){
-            if(arrForMinAndMax[i] > max){
-                max = arrForMinAndMax[i];
-            }
-            else if(arrForMinAndMax[i] < min){
-                min = arrForMinAndMax[i];
+                System.out.println("Снова угадайте число от 1 до 9. У Вас по-прежнему 3 попытки.");
             }
         }
-        System.out.println("Максимум массива: " + max + ", а минимум: " + min);
-
-        System.out.println("- - - - - - - - - -");
-
-        int[] a = {2, 2, 2, 1, 2, 2, 10, 1};
-        int[] b = {2, 2, 2, 1, 2, 2, 10, 2};
-        System.out.println(checkArrayBalance(a));
-        System.out.println(checkArrayBalance(b));
-
-        System.out.println("- - - - - - - - - -");
-        int[] testArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        arrayShift(testArray, 3);
     }
 
-    //task6
-    public static boolean checkArrayBalance(int[] a){
-        for(int i = 0; i < a.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < i; j++) {
-                sum += a[j];
-            }
-            for (int j = i; j < a.length; j++) {
-                sum -= a[j];
-            }
-            if (sum == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
+    public static void guessRandomWords(){
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon",
+                "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
-    //task7
-    public static void arrayShift(int[] arr, int n){
-        if(n < 0){
-            for(int i = 0; i < n; i++) {
-                int tmp = arr[0];
-                for (int j = 0; j < arr.length - 1; j++) {
-                    arr[j] = arr[j + 1];
-                }
-                arr[arr.length - 1] = tmp;
-            }
-        }else{
-            for(int i = 0; i < n; i++) {
-                int tmp = arr[0];
-                arr[0] = arr[arr.length - 1];
-                for (int j = 1; j < arr.length - 1; j++) {
-                    arr[arr.length - j] = arr[arr.length - j - 1];
-                }
-                arr[1] = tmp;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-    }
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        String randomWord = words[random.nextInt(26)-1];
+        System.out.println("Угадайте англо-язычное слово, обозначающее одно из культурных растений");
 
+        while(true) {
+            String inputWord = scan.nextLine();
+            String outWord = "";
+            if(randomWord.equals(inputWord)){
+                System.out.println("Вы угадали слово!");
+                break;
+            }
+
+            for (int i = 0; i < randomWord.length(); i++) {
+                if (randomWord.charAt(i) == inputWord.charAt(i)) {
+                    outWord += randomWord.charAt(i);
+                } else {
+                    outWord += "#";
+                }
+            }
+            for (int i = 0; i < (15 - inputWord.length()); i++) {
+                outWord += "#";
+            }
+            System.out.println("Подсказка: " + outWord);
+            System.out.println("Попробуйте угадать ещё раз:");
+        }
+    }
 }
