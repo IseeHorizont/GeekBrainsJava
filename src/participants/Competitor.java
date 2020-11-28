bb34a0a716bd83793ebe1f765c6555f78f22de98
IseeHorizont <68839013+IseeHorizont@users.noreply.github.com>
2020-11-28 18:34:35 +1000

    Delete Competitor.java

--- src/participants/Competitor.java
+++
@@ -1,9 +0,0 @@
-package participants;
-
-public interface Competitor {
-    void run(int distance);
-    void swim(int distance);
-    void jump(int height);
-    boolean isOnDistance();
-    void showResult();
-}
